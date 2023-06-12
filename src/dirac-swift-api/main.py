83d3c9dd43022da5a378b14af8d18b05b5b2c148
Harry Moss <h.moss@ucl.ac.uk>
2023-06-12 17:36:39 +0100

    FastAPI scaffolding

--- src/dirac-swift-api/main.py
+++ src/dirac-swift-api/main.py
@@ -1,1 +1,28 @@
-"""An example file."""
+"""Entry point and main file for the FastAPI backend."""
+from fastapi import APIRouter, FastAPI
+from fastapi.responses import ORJSONResponse
+from loguru import logger
+
+logger.info("API starting")
+
+app = FastAPI(
+    default_response_class=ORJSONResponse,
+)
+
+# Define a mock router for testing
+mock_router = APIRouter(
+    prefix="/mock",
+)
+
+# Include the mock router in the main router
+app.include_router(mock_router)
+
+
+@app.get("/ping")
+def ping() -> dict[str, str]:
+    """Define an API route for testing purposes.
+
+    Returns:
+        dict[str, str]: Some example content
+    """
+    return {"ping": "pong"}
